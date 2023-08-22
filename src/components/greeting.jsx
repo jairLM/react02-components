9372ebfebcbc5a1446cca9296e8f351c704e8100
Jair Murillo <brook_98@live.com>
2023-08-22 14:04:16 -0600

    creacion de greeting

---
+++ src/components/greeting.jsx
@@ -0,0 +1,14 @@
+
+//functional component
+/*
+los componentes funcionales son funciones de JS que devuelven JSX.
+Estas funciones tienen una sintaxis más simple y concisa
+ */
+export default function Greeting(){
+    return (
+    <div className="App">
+        <h1>Felicidades Generation por tu 8o Aniversario</h1>
+        <h2>Pueden llegar a la pachanga</h2>
+    </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Greeting from './components/greeting';//importamos una funcion que se exporto como default
+/**
+ * React.strictMode: componente de react que se utiliza en el desarollo
+ * para detectar y advertir sobre posibles problemas en una aplicación
+ */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>  
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
